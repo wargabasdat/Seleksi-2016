c6e5808829b1a8a3f8131cc58e7c9ac9c30bd421
Martino Christanto Khuangga <martino.aksel.11@gmail.com>
2016-06-18 00:05:42 +0700

    Update Visualisasi Pengelompokan Gaji

--- Pengelompokan Gaji.R
+++ Pengelompokan Gaji.R
@@ -1,14 +1,14 @@
 library(ggplot2)
 
 # Load data
-myData = read.csv("Salaries.csv")
+mydata = read.csv("Salaries.csv")
 
 # Pengelompokan Gaji (Rendah, Sedang, Tinggi)
 # Pengelompokan Rendah (0,mean-stdev)
 # Pengelompokan Sedang (mean-stdev,mean+stdev)
 # Pengelompokan Tinggi (>mean+stdev)
-tes <- myData$TotalPayBenefits
+tes <- mydata$TotalPayBenefits
 Pengelompokan_gaji <- cut(tes, c(0,mean(tes)-sd(tes),mean(tes)+sd(tes),max(tes)), include.lowest=TRUE, include.highest=TRUE, labels = c("Rendah","Sedang","Tinggi"))
 
 # Visualisasi data
-qplot(Pengelompokan_gaji, data = mydata, geom = "bar", colour = Pengelompokan_gaji)
+ggsave("~/Desktop/Pengelompokan_Gaji.png", plot = qplot(Pengelompokan_gaji, data = mydata, geom = "bar", fill = Pengelompokan_gaji, main = "Pengelompokan Gaji San Fransisco"))
