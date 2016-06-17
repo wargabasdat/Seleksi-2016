2980f8aab1fdee37b0f01e1cd58580055becc1ea
Isham Faizal <ishamf@users.noreply.github.com>
2016-06-17 14:30:40 +0700

    Fixed conversion script

--- Convert.py
+++ Convert.py
@@ -1,15 +1,15 @@
 import sys
 import pandas as pd
 
-dfs = pd.read_csv(sys.argv[1], chunksize=100)
+dfs = pd.read_csv(sys.argv[1], index_col=0, chunksize=100)
 
-with open(sys.argv[2], 'a') as f:
+with open(sys.argv[2], 'w') as f:
     for df in dfs:
-        sdf = pd.to_numeric(df.POLYLINE
-                              .str.split(',', expand=True)
-                              .stack()
-                              .str.replace('(41\.)|(-8\.)|\[|\]', '')
-                              .str.ljust(6, '0'))
+        pol = df.POLYLINE
+        cpl = pol[~(pol == '[]')]
+        sdf = pd.to_numeric(cpl.str.split(',', expand=True)
+                               .stack()
+                               .str.replace('\[|\]', ''))
         odds = sdf[1::2]
         evens = sdf[::2]
         odds.index = pd.MultiIndex.from_tuples(odds.index.map(lambda x: (x[0], (x[1]-1)//2)))
