3434adb1a20ea4a13aa18852dcf252a802ac5397
ratnadiraw <ratnadiraw@gmail.com>
2016-06-19 16:45:59 +0700

    Update soalnomer3.py

--- soalnomer3.py
+++ soalnomer3.py
@@ -5,13 +5,19 @@ import csv
 
 FILE='test.csv'
 OUTPUTF='Rata-rata waktu tempuh.csv'
-
+#baca file
 data = pandas.read_csv(FILE)
+#cari nilai koordinat selain nilai awal, tiap perpindahan koordinat 15 detik
 pindah = 0
+simpan = 0
 for p in data['POLYLINE']:
-	pindah = pindah + (((p.count('['))-2)*15)
-waktu = pindah / len(data['POLYLINE'])
-
+	if (p.count('[')!=0):
+		pindah = pindah + (((p.count('['))-2)*15)
+	else:
+		simpan = simpan + 1
+#nilainya dibagi dengan panjang data yang tidak hilang data GPSnya
+waktu = pindah / (len(data['POLYLINE']) - simpan)
+#simpan nilainya ke file output
 with open(OUTPUTF, 'w', newline='') as fl:
 	tulisf = csv.writer(fl, delimiter=',')
 	tulisf.writerow(['Rata-rata waktu tempuh'])
