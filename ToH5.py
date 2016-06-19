a99cc4bfa0a9646a6a76398025e3f23c70f401fa
Isham Faizal <ishamf@users.noreply.github.com>
2016-06-19 12:50:14 +0700

    Added cleanup code

--- ToH5.py
+++ ToH5.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 df = pd.read_csv(sys.argv[1], index_col=(0, 1), names=['lat', 'lon'])
 
+df = df.reset_index()
+df = df.drop_duplicates(subset=['level_0', 'level_1'])
+df = df.set_index(['level_0', 'level_1'])
+
 df.to_hdf(sys.argv[2], 'data')
