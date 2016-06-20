b3f7d92ec125a9189feb31348af341e804f78fd6
ratnadiraw <ratnadiraw@gmail.com>
2016-06-20 10:29:10 +0700

    Update soalnomer3.py

--- soalnomer3.py
+++ soalnomer3.py
@@ -10,11 +10,14 @@ data = pandas.read_csv(FILE)
 #cari nilai koordinat selain nilai awal, tiap perpindahan koordinat 15 detik
 pindah = 0
 simpan = 0
+#catat perpindahan yang dilakukan, apabila data gps tidak lengkap, tidak dimasukan 
+#ke perhitungan
 for p in data['POLYLINE']:
-	if (p.count('[')!=0):
+	if (data['MISSING_DATA'][save]==False):
 		pindah = pindah + (((p.count('['))-2)*15)
 	else:
 		simpan = simpan + 1
+	save=save+1
 #nilainya dibagi dengan panjang data yang tidak hilang data GPSnya
 waktu = pindah / (len(data['POLYLINE']) - simpan)
 #simpan nilainya ke file output
