54d878765879332526966e68c4b47ab3d996d2bd
edriajulianata <edriajulianata@gmail.com>
2016-06-20 14:02:36 +0700

    Add files via upload

--- gaji.R
+++ gaji.R
@@ -32,35 +32,97 @@ ggplot(data=rata2,aes(x = Year, y = TotalPayBenefits)) +
 #########
 #Nomor 2
 #Pengelompokan pendapatan dari yang rendah hingga tinggi
-Salaries.Wow <- Salaries
-Salaries.Wow$Wew = "Rendah"
-Salaries.Wow[which(Salaries.Wow$TotalPayBenefits > 75000), 'Wew'] = "Menengah"
-Salaries.Wow[which(Salaries.Wow$TotalPayBenefits > 160000), 'Wew'] = "Tinggi"
+Salaries.Job <- Salaries
+Salaries.Job$Group = "Rendah"
+Salaries.Job[which(Salaries.Job$TotalPayBenefits > 75000), 'Group'] = "Menengah"
+Salaries.Job[which(Salaries.Job$TotalPayBenefits > 160000), 'Group'] = "Tinggi"
 
 #Histogram
-Salaries.Wow %>%
-  ggplot(aes(x=factor(Wew, level=c('Rendah', 'Menengah', 'Tinggi')))) +
+Salaries.Job %>%
+  ggplot(aes(x=factor(Group, level=c('Rendah', 'Menengah', 'Tinggi')))) +
   geom_bar() +
   ggtitle("Pengelompokan Berdasarkan Besar Pendapatan") +
   ylab("Jumlah") +
   xlab("Tingkat")
 
-table(Salaries.Wow$Wew)
+table(Salaries.Job$Group)
 
 #unique(as.character(Salaries$JobTitle))
 
 #unique(as.character(Salaries$JobTitle))
 
 #No 3
-Salaries.Job <- Salaries
+Salaries.Job$JobTitle <- toupper(Salaries.Job$JobTitle)
+
+
+Salaries.Job$Job = "Other"
 Salaries.Job[which(grepl('FIRE',Salaries.Job$JobTitle)), 'Job'] = "Fire"
 Salaries.Job[which(grepl('POLICE',Salaries.Job$JobTitle)), 'Job'] = "Police"
+Salaries.Job[which(grepl('SHERIF',Salaries.Job$JobTitle)), 'Job'] = "Police"
+Salaries.Job[which(grepl('PROBATION',Salaries.Job$JobTitle)), 'Job'] = "Police"
+Salaries.Job[which(grepl('SERGEANT',Salaries.Job$JobTitle)), 'Job'] = "Police"
+Salaries.Job[which(grepl('MTA',Salaries.Job$JobTitle)), 'Job'] = "Transit"
+Salaries.Job[which(grepl('TRANSIT',Salaries.Job$JobTitle)), 'Job'] = "Transit"
+Salaries.Job[which(grepl('ANESTH',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('MEDICAL',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('NURS',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('PHYSICIAN',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('HEALTH',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('ORTHOPEDIC',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('HEALTH',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('PHARM',Salaries.Job$JobTitle)), 'Job'] = "Medical"
+Salaries.Job[which(grepl('DENTIST',Salaries.Job$JobTitle)), 'Job'] = "Medical"
 
+Salaries.Job[which(grepl('AIRPORT',Salaries.Job$JobTitle)), 'Job'] = "Airport"
+Salaries.Job[which(grepl('ANIMAL',Salaries.Job$JobTitle)), 'Job'] = "Animal"
+Salaries.Job[which(grepl('ARCHITECT',Salaries.Job$JobTitle)), 'Job'] = "Architectural"
+Salaries.Job[which(grepl('COURT',Salaries.Job$JobTitle)), 'Job'] = "Court"
+Salaries.Job[which(grepl('LEGAL',Salaries.Job$JobTitle)), 'Job'] = "Court"
+#Salaries.Job[which(grepl('DEFENDER',Salaries.Job$JobTitle)), 'Job'] = "Court"
+
+Salaries.Job[which(grepl('MAYOR',Salaries.Job$JobTitle)), 'Job'] = "Mayor"
+Salaries.Job[which(grepl('LIBRAR',Salaries.Job$JobTitle)), 'Job'] = "Library"
+Salaries.Job[which(grepl('PARKING',Salaries.Job$JobTitle)), 'Job'] = "Parking"
+Salaries.Job[which(grepl('PUBLIC WORKS',Salaries.Job$JobTitle)), 'Job'] = "Public Works"
+#Salaries.Job[which(grepl('INCIDENT',Salaries.Job$JobTitle)), 'Job'] = "Public Works"
+
+Salaries.Job[which(grepl('ATTORNEY',Salaries.Job$JobTitle)), 'Job'] = "Attorney"
+Salaries.Job[which(grepl('MECHANIC',Salaries.Job$JobTitle)), 'Job'] = "Automotive"
+Salaries.Job[which(grepl('AUTOMOTIVE',Salaries.Job$JobTitle)), 'Job'] = "Automotive"
+Salaries.Job[which(grepl('CUSTODIAN',Salaries.Job$JobTitle)), 'Job'] = "Custodian"
+Salaries.Job[which(grepl('ENGINEER',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+Salaries.Job[which(grepl('ENGR',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+Salaries.Job[which(grepl('ACCOUNT',Salaries.Job$JobTitle)), 'Job'] = "Accounting"
+Salaries.Job[which(grepl('GARDENER',Salaries.Job$JobTitle)), 'Job'] = "Gardening"
+Salaries.Job[which(grepl('GENERAL LABORER',Salaries.Job$JobTitle)), 'Job'] = "General"
+Salaries.Job[which(grepl('FOOD SERV',Salaries.Job$JobTitle)), 'Job'] = "Food Service"
+Salaries.Job[which(grepl('CLERK',Salaries.Job$JobTitle)), 'Job'] = "Clerk"
+Salaries.Job[which(grepl('PORTER',Salaries.Job$JobTitle)), 'Job'] = "Porter"
+#others
+Salaries.Job[which(grepl('DEPUTY',Salaries.Job$JobTitle)), 'Job'] = "Politics"
+Salaries.Job[which(grepl('MANAGER',Salaries.Job$JobTitle)), 'Job'] = "Manager"
+Salaries.Job[which(grepl('TECHNICIAN',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+Salaries.Job[which(grepl('CONTROLLER',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+Salaries.Job[which(grepl('ELECTRIC',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+Salaries.Job[which(grepl('PLUMBER',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+Salaries.Job[which(grepl('TECH',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+
+
+Salaries.Job[which(grepl('DEPARTMENT',Salaries.Job$JobTitle)), 'Job'] = "Politics"
+Salaries.Job[which(grepl('EMPLOYEE',Salaries.Job$JobTitle)), 'Job'] = "Public Works"
+Salaries.Job[which(grepl('MAINTENANCE',Salaries.Job$JobTitle)), 'Job'] = "Engineering"
+#Salaries.Job[which(grepl('ASSISTANT',Salaries.Job$JobTitle)), 'Job'] = "Employee"
+#Salaries.Job[which(grepl('WORK',Salaries.Job$JobTitle)), 'Job'] = "Employee"
+
+#Salaries.Job[which(grepl('DIRECTOR',Salaries.Job$JobTitle)), 'Job'] = "Director"
 
 #Histogram
 Salaries.Job %>%
   ggplot(aes(x=Job)) +
-  geom_bar(aes(fill=factor(JobTitle))) + 
-  ggtitle("Pengelompokan Berdasarkan Besar Pendapatan") +
+  geom_bar() + #aes(fill=factor(JobTitle))
+  ggtitle("Pengelompokan Berdasarkan Bidang Pekerjaan") +
   ylab("Jumlah") +
-  xlab("Tingkat")
+  xlab("Tingkat") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+table(Salaries.Job$Job)
